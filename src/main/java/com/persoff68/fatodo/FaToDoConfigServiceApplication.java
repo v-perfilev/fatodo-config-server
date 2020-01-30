ace2c3af0f340efca96209cc1217a7866fde1b9d
Vladimir Perfilev <persoff68@gmail.com>
2020-01-30 08:42:37 +0100

    setup app

--- src/main/java/com/persoff68/fatodo/FaToDoConfigServiceApplication.java
+++ src/main/java/com/persoff68/fatodo/FaToDoConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.persoff68.fatodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class FaToDoConfigServiceApplication {
 
     public static void main(String[] args) {
